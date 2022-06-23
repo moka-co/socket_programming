5e88c42a2d6f2ee1faa35c498414cc8ae6abab6a
mokassino <prokurush@gmail.com>
2022-06-23 18:59:13 +0200

    added simple concurrent server

---
+++ client_server_simple_fork/client.c
@@ -0,0 +1,36 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "wrapped.h"
+
+
+#define BUFSIZE 1024
+
+int main(int argc, char *argv[]){
+
+        int err, sockfd;
+        socklen_t len;
+        struct sockaddr_in address; //address
+
+        char buffer[BUFSIZE];
+
+        struct sockaddr_in * ptr_address = &address;
+
+        Socket(&sockfd);
+
+        makeSockaddr(ptr_address,"127.0.0.1", 9735, &len);
+
+        Connect(sockfd, ptr_address, len);
+
+        FullRead(sockfd, buffer, BUFSIZE);
+
+        printf("%s", buffer);
+        Close(sockfd);
+
+
+        exit(0);
+}
